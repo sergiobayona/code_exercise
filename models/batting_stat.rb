b0a46d9029ce16dfe2312e3d4f95c63c3f1d372f
Sergio Bayona <sbayona@martianinteractive.com>
2014-08-25 22:30:35 -0400

    scope for batting criteria

--- models/batting_stat.rb
+++ models/batting_stat.rb
@@ -1,6 +1,13 @@
 class BattingStat < ActiveRecord::Base
   belongs_to :player, primary_key: :player_id
 
+  scope :batting_criteria, ->(years, batting_above) {
+    select("*, SUM(at_bats) AS at_bats_aggregate, SUM(hits) AS hits_aggregate")
+    .where(year: years)
+    .where(self.arel_table[:at_bats].gt(batting_above))
+    .group(:year, :player_id)
+  }
+
 
   def slugging_percentage
     at_bats.to_i > 0 ? (100.0 * (hits + 2*doubles + 3*triples + 4*home_runs) / at_bats) : 0.0
